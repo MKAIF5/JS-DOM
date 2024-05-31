7fd3bd3a8562d644ddb3a8fd397cd76583da362c
mohammed kaif <mkaif555123@gmail.com>
2024-05-31 20:31:51 +0500

    dom started

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-//Dom means document object model
-//Bom means browser object model
+// //Dom means document object model
+// //Bom means browser object model
 
-console.log(window);
-console.log(document);
+// // console.log(window);
+// console.log(document.body);
+
+let dom = document.getElementById("title").className
+
+console.log(dom);
