0e8610e4ed5fe98dff521e25bae35b0b85c0682e
mohammed kaif <mkaif555123@gmail.com>
2024-07-03 17:05:55 +0500

    DOM Practice

--- script.js
+++ script.js
@@ -1,9 +1,50 @@
 // //Dom means document object model
-// //Bom means browser object model
 
 // // console.log(window);
 // console.log(document.body);
 
-let dom = document.getElementById("title").className
+// DOM
 
-console.log(dom);
+const title = document.getElementById("title")
+
+// title.style.backgroundColor = "green"
+// title.style.height = "100px"
+// title.style.padding = "10px"
+// title.style.borderRadius = "10px"
+
+// const a = title.innerHTML
+// const b = title.innerText
+// const c = title.textContent
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+//Dom Create new element
+
+const parent = document.querySelector(".parent");
+
+// console.log(parent.children[0].innerHTML);
+
+// for (i = 0; i < parent.children.length; i++) {
+//     console.log(parent.children[i].innerHTML);
+// }
+
+// parent.children[1].style.color = "orange";
+// console.log(parent.firstElementChild);
+// console.log(parent.lastElementChild);
+
+// const days = document.querySelector(".day");
+
+// console.log(days.parentElement);
+// console.log(days.nextElementSibling);
+
+// console.log("NODES: " , parent.childNodes);
+
+const div = document.createElement("h1");
+div.className = "main"
+div.id = Math.round(Math.random() * 10 + 1)
+div.setAttribute("title", "kaif")
+// div.innerText = "practice DOM"
+const addText = document.createTextNode("parctice DOM");
+div.appendChild(addText)
+console.log(div);
